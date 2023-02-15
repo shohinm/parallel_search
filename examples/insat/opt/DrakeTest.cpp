1971e532636cb809b4ec0719cb0c370a7ab7cdce
nrkumar93 <nrkumar93@gmail.com>
2023-02-15 18:08:44 -0500

    after success with python started porting to c++

--- examples/insat/opt/DrakeTest.cpp
+++ examples/insat/opt/DrakeTest.cpp
@@ -1,4 +1,58 @@
+// PS
+#include <common/Types.hpp>
+
+// Drake
 #include <drake/systems/trajectory_optimization/kinematic_trajectory_optimization.h>
+
+namespace ps
+{
+    struct ABBParams
+    {
+        ABBParams()
+        {
+            min_q_ << -3.14159, -1.0995, -4.1015, -3.4906, -2.0071, -6.9813;
+            max_q_ << 3.14159, 1.9198, 0.9599, 3.4906, 2.0071, 6.9813;
+            min_dq_ << -2.618, -2.7925, -2.967, -5.585, -6.9813, -7.854;
+            max_dq_ << 2.618, 2.7925, 2.967, 5.585, 6.9813, 7.854;
+        }
+
+        VecDf min_q_, max_q_, min_dq_, max_dq_;
+    };
+
+    class BSplineOpt
+    {
+    public:
+        typedef drake::systems::trajectory_optimization::KinematicTrajectoryOptimization OptType;
+        typedef drake::solvers::Binding<drake::solvers::Cost> CostType;
+        typedef drake::solvers::Binding<drake::solvers::Constraint> ConstraintType;
+
+        BSplineOpt(int num_positions, int num_control_points,
+                   int spline_order = 4, double duration = 1.0)
+        {
+            opt_ = std::make_shared<OptType>(num_positions, num_control_points, spline_order, duration);
+
+        }
+
+        int clearCosts()
+        {
+
+        }
+
+        int clearConstraints()
+        {
+
+        }
+
+        /// Optimizer
+        std::shared_ptr<OptType> opt_;
+        drake::solvers::MathematicalProgram prog_;
+
+        std::vector<CostType> costs_;
+        std::vector<ConstraintType> constraints_;
+    };
+
+}
+
 
 int main()
 {
