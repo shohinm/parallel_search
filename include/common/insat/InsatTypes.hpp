fe81fd837d8857607fdee4cd5035d3b8ff357b35
Ramkumar Natarajan <nrkumar93@gmail.com>
2023-02-19 14:30:30 -0500

    commenting out InsatTypes which depends on drake so that manipulation actions builds successfully

--- include/common/insat/InsatTypes.hpp
+++ include/common/insat/InsatTypes.hpp
@@ -6,32 +6,32 @@
 #define INSATTYPES_HPP
 
 // Drake
-#include <drake/planning/trajectory_optimization/kinematic_trajectory_optimization.h>
-#include <drake/solvers/solve.h>
-
-#include <common/EigenTypes.h>
-
-namespace ps
-{
-
-    struct InsatParams
-    {
-        int lowD_dims_ = 6;
-        int fullD_dims_ = 12;
-        int aux_dims_ = 6;
-    };
-
-    struct BSplineTraj
-    {
-        typedef drake::solvers::MathematicalProgramResult OptResultType;
-
-        inline long size() {return traj_.size();}
-
-        OptResultType result_;
-        MatDf traj_;
-    };
-
-}
+//#include <drake/planning/trajectory_optimization/kinematic_trajectory_optimization.h>
+//#include <drake/solvers/solve.h>
+//
+//#include <common/EigenTypes.h>
+//
+//namespace ps
+//{
+//
+//    struct InsatParams
+//    {
+//        int lowD_dims_ = 6;
+//        int fullD_dims_ = 12;
+//        int aux_dims_ = 6;
+//    };
+//
+//    struct BSplineTraj
+//    {
+//        typedef drake::solvers::MathematicalProgramResult OptResultType;
+//
+//        inline long size() {return traj_.size();}
+//
+//        OptResultType result_;
+//        MatDf traj_;
+//    };
+//
+//}
 
 
 #endif //INSATTYPES_HPP
