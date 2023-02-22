f8277debcf26054f0e9c021593cd054ecbf3e136
nrkumar93 <nrkumar93@gmail.com>
2023-02-21 20:57:00 -0500

    insat params constructor

--- include/common/insat/InsatTypes.hpp
+++ include/common/insat/InsatTypes.hpp
@@ -16,19 +16,29 @@ namespace ps
 
     struct InsatParams
     {
+        InsatParams(int lowD_dims, int fullD_dims_, int aux_dims) : lowD_dims_(lowD_dims),
+                                                                    fullD_dims_(fullD_dims_),
+                                                                    aux_dims_(aux_dims) {}
+
         int lowD_dims_ = 6;
         int fullD_dims_ = 12;
         int aux_dims_ = 6;
     };
 
-    struct BSplineTraj : public MatDf
+    struct DummyTraj
+    {
+        MatDf disc_traj_;
+    };
+
+    struct BSplineTraj
     {
         typedef drake::solvers::MathematicalProgramResult OptResultType;
+        typedef drake::trajectories::BsplineTrajectory<double> TrajInstanceType;
 
         inline long size() {return disc_traj_.size();}
 
         OptResultType result_;
-        drake::trajectories::BsplineTrajectory<double> traj_;
+        TrajInstanceType traj_;
         MatDf disc_traj_;
     };
 
