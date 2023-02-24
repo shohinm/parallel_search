4036cb86c2e187b3bec31ae9461970a50308cd55
shohin <shohin.93@gmail.com>
2023-02-24 10:25:55 -0500

    Removed hardcoding of planner and threads

--- examples/manipulation/run_manipulation.cpp
+++ examples/manipulation/run_manipulation.cpp
@@ -244,25 +244,22 @@ int main(int argc, char* argv[])
 {
     int num_threads;
 
-//   if (!strcmp(argv[1], "insat"))
-//   {
-//       if (argc != 2) throw runtime_error("Format: run_robot_nav_2d insat");
-//       num_threads = 1;
-//   }
-//   else if (!strcmp(argv[1], "pinsat") || !strcmp(argv[1], "rrt") || !strcmp(argv[1], "rrtconnect"))
-//   {
-//       if (argc != 3) throw runtime_error("Format: run_robot_nav_2d pinsat [num_threads]");
-//       num_threads = atoi(argv[2]);
-//   }
-//   else
-//   {
-//       throw runtime_error("Planner " + string(argv[1]) + " not identified");
-//   }
-//
-//   string planner_name = argv[1];
+    if (!strcmp(argv[1], "insat"))
+    {
+      if (argc != 2) throw runtime_error("Format: run_robot_nav_2d insat");
+      num_threads = 1;
+    }
+    else if (!strcmp(argv[1], "pinsat") || !strcmp(argv[1], "rrt") || !strcmp(argv[1], "rrtconnect"))
+    {
+      if (argc != 3) throw runtime_error("Format: run_robot_nav_2d pinsat [num_threads]");
+      num_threads = atoi(argv[2]);
+    }
+    else
+    {
+      throw runtime_error("Planner " + string(argv[1]) + " not identified");
+    }
 
-     num_threads = 1;
-     std::string planner_name = "insat";
+    string planner_name = argv[1];
 
     /// Load MuJoCo model
     std::string modelpath = "../third_party/mujoco-2.3.2/model/abb/irb_1600/irb1600_6_12_shield.xml";
