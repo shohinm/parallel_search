3e6b13bc366de4ea7b817fd885b67caf27b3b52f
shohin <shohin.93@gmail.com>
2023-02-26 23:24:06 -0500

    Added epase and rrt to analyze

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -33,29 +33,29 @@ def sanity_check(data, cost_thresh):
     return result
 
 
-def print_stats(insat, pinsat, insat_adaptive, pinsat_adaptive, cost_thresh):
+def print_stats(insat, pinsat, rrt, epase, cost_thresh):
     print("\n\n--------------- Cost thresh: {} ---------------".format(cost_thresh))
 
     d = {}
     d['insat'] = {'id' : insat[:, 0], 'time' : insat[:, 1], 'cost' : insat[:, 2], 'length' : insat[:, 3], 'state_expansions' : insat[:, 4], 'edge_expansions' : insat[:, 5]} 
     d['pinsat'] = {'id' : pinsat[:, 0], 'time' : pinsat[:, 1], 'cost' : pinsat[:, 2], 'length' : pinsat[:, 3], 'state_expansions' : pinsat[:, 4], 'edge_expansions' : pinsat[:, 5]} 
-    d['insat_adaptive'] = {'id' : insat_adaptive[:, 0], 'time' : insat_adaptive[:, 1], 'cost' : insat_adaptive[:, 2], 'length' : insat_adaptive[:, 3], 'state_expansions' : insat_adaptive[:, 4], 'edge_expansions' : insat_adaptive[:, 5]} 
-    d['pinsat_adaptive'] = {'id' : pinsat_adaptive[:, 0], 'time' : pinsat_adaptive[:, 1], 'cost' : pinsat_adaptive[:, 2], 'length' : pinsat_adaptive[:, 3], 'state_expansions' : pinsat_adaptive[:, 4], 'edge_expansions' : pinsat_adaptive[:, 5]} 
+    d['rrt'] = {'id' : rrt[:, 0], 'time' : rrt[:, 1], 'cost' : rrt[:, 2], 'length' : rrt[:, 3], 'state_expansions' : rrt[:, 4], 'edge_expansions' : rrt[:, 5]} 
+    d['epase'] = {'id' : epase[:, 0], 'time' : epase[:, 1], 'cost' : epase[:, 2], 'length' : epase[:, 3], 'state_expansions' : epase[:, 4], 'edge_expansions' : epase[:, 5]} 
 
     insat_idx, insat_success, insat_success_idx = relevant_indices('insat', d['insat'], cost_thresh, 1000)
-    insat_adaptive_idx, insat_adaptive_success, insat_adaptive_success_idx = relevant_indices('insat_adaptive', d['insat_adaptive'], cost_thresh, 1000)
+    insat_adaptive_idx, insat_adaptive_success, insat_adaptive_success_idx = relevant_indices('rrt', d['rrt'], cost_thresh, 1000)
     pinsat_idx, pinsat_success, pinsat_success_idx = relevant_indices('pinsat', d['pinsat'], cost_thresh, 1000)
-    pinsat_adaptive_idx, pinsat_adaptive_success, pinsat_adaptive_success_idx = relevant_indices('pinsat_adaptive', d['pinsat_adaptive'], cost_thresh, 1000)
+    pinsat_adaptive_idx, pinsat_adaptive_success, pinsat_adaptive_success_idx = relevant_indices('epase', d['epase'], cost_thresh, 1000)
 
     insat = insat[insat_idx, :]
     pinsat = pinsat[insat_idx, :]
-    insat_adaptive = insat_adaptive[insat_adaptive_idx, :]
-    pinsat_adaptive = pinsat_adaptive[pinsat_adaptive_idx, :]
+    rrt = rrt[insat_adaptive_idx, :]
+    epase = epase[pinsat_adaptive_idx, :]
 
     
     common_idx = np.intersect1d(insat[:,0], pinsat[:,0])
-    common_idx = np.intersect1d(common_idx, insat_adaptive[:,0])
-    common_idx = np.intersect1d(common_idx, pinsat_adaptive[:,0])
+    common_idx = np.intersect1d(common_idx, rrt[:,0])
+    common_idx = np.intersect1d(common_idx, epase[:,0])
     
     # pdb.set_trace()
 
@@ -69,20 +69,20 @@ def print_stats(insat, pinsat, insat_adaptive, pinsat_adaptive, cost_thresh):
 
         insat_filtered.append(insat[np.where(insat[:,0] == idx)])
         pinsat_filtered.append(pinsat[np.where(pinsat[:,0] == idx)])
-        insat_adaptive_filtered.append(insat_adaptive[np.where(insat_adaptive[:,0] == idx)])
-        pinsat_adaptive_filtered.append(pinsat_adaptive[np.where(pinsat_adaptive[:,0] == idx)])
+        insat_adaptive_filtered.append(rrt[np.where(rrt[:,0] == idx)])
+        pinsat_adaptive_filtered.append(epase[np.where(epase[:,0] == idx)])
     
 
     insat = np.array(insat_filtered).squeeze()
     pinsat = np.array(pinsat_filtered).squeeze()
-    insat_adaptive = np.array(insat_adaptive_filtered).squeeze()
-    pinsat_adaptive = np.array(pinsat_adaptive_filtered).squeeze()
+    rrt = np.array(insat_adaptive_filtered).squeeze()
+    epase = np.array(pinsat_adaptive_filtered).squeeze()
 
 
     d['insat'] = {'id' : insat[:, 0], 'time' : insat[:, 1], 'cost' : insat[:, 2], 'length' : insat[:, 3], 'state_expansions' : insat[:, 4], 'edge_expansions' : insat[:, 5]} 
     d['pinsat'] = {'id' : pinsat[:, 0], 'time' : pinsat[:, 1], 'cost' : pinsat[:, 2], 'length' : pinsat[:, 3], 'state_expansions' : pinsat[:, 4], 'edge_expansions' : pinsat[:, 5]} 
-    d['insat_adaptive'] = {'id' : insat_adaptive[:, 0], 'time' : insat_adaptive[:, 1], 'cost' : insat_adaptive[:, 2], 'length' : insat_adaptive[:, 3], 'state_expansions' : insat_adaptive[:, 4], 'edge_expansions' : insat_adaptive[:, 5]} 
-    d['pinsat_adaptive'] = {'id' : pinsat_adaptive[:, 0], 'time' : pinsat_adaptive[:, 1], 'cost' : pinsat_adaptive[:, 2], 'length' : pinsat_adaptive[:, 3], 'state_expansions' : pinsat_adaptive[:, 4], 'edge_expansions' : pinsat_adaptive[:, 5]} 
+    d['rrt'] = {'id' : rrt[:, 0], 'time' : rrt[:, 1], 'cost' : rrt[:, 2], 'length' : rrt[:, 3], 'state_expansions' : rrt[:, 4], 'edge_expansions' : rrt[:, 5]} 
+    d['epase'] = {'id' : epase[:, 0], 'time' : epase[:, 1], 'cost' : epase[:, 2], 'length' : epase[:, 3], 'state_expansions' : epase[:, 4], 'edge_expansions' : epase[:, 5]} 
 
 
     # pdb.set_trace()
@@ -94,7 +94,7 @@ def print_stats(insat, pinsat, insat_adaptive, pinsat_adaptive, cost_thresh):
 
 
 
-    stats = {'insat': {}, 'pinsat': {}, 'insat_adaptive': {}, 'pinsat_adaptive': {}}
+    stats = {'insat': {}, 'pinsat': {}, 'rrt': {}, 'epase': {}}
 
 
     stats['insat']['num_success_problems'] = d['insat']['id'].shape[0]
@@ -119,30 +119,30 @@ def print_stats(insat, pinsat, insat_adaptive, pinsat_adaptive, cost_thresh):
     stats['pinsat']['mean_state_expansions'] = np.mean(d['pinsat']['state_expansions'])
     stats['pinsat']['mean_edge_expansions'] = np.mean(d['pinsat']['edge_expansions'])
 
-    stats['insat_adaptive']['num_success_problems'] = d['insat_adaptive']['id'].shape[0]
-    stats['insat_adaptive']['success_rate'] = insat_adaptive_success
-    stats['insat_adaptive']['mean_cost'] = np.mean(d['insat_adaptive']['cost'])
-    stats['insat_adaptive']['std_cost'] = np.std(d['insat_adaptive']['cost'])
-    stats['insat_adaptive']['mean_time'] = np.mean(d['insat_adaptive']['time'])
-    stats['insat_adaptive']['std_time'] = np.std(d['insat_adaptive']['time'])
-    stats['insat_adaptive']['median_time'] = np.median(d['insat_adaptive']['time'])
-    stats['insat_adaptive']['max_time'] = np.max(d['insat_adaptive']['time'])
-    stats['insat_adaptive']['mean_state_expansions'] = np.mean(d['insat_adaptive']['state_expansions'])
-    stats['insat_adaptive']['mean_edge_expansions'] = np.mean(d['insat_adaptive']['edge_expansions'])
+    stats['rrt']['num_success_problems'] = d['rrt']['id'].shape[0]
+    stats['rrt']['success_rate'] = insat_adaptive_success
+    stats['rrt']['mean_cost'] = np.mean(d['rrt']['cost'])
+    stats['rrt']['std_cost'] = np.std(d['rrt']['cost'])
+    stats['rrt']['mean_time'] = np.mean(d['rrt']['time'])
+    stats['rrt']['std_time'] = np.std(d['rrt']['time'])
+    stats['rrt']['median_time'] = np.median(d['rrt']['time'])
+    stats['rrt']['max_time'] = np.max(d['rrt']['time'])
+    stats['rrt']['mean_state_expansions'] = np.mean(d['rrt']['state_expansions'])
+    stats['rrt']['mean_edge_expansions'] = np.mean(d['rrt']['edge_expansions'])
 
-    stats['pinsat_adaptive']['num_success_problems'] = d['pinsat_adaptive']['id'].shape[0]
-    stats['pinsat_adaptive']['success_rate'] = pinsat_adaptive_success
-    stats['pinsat_adaptive']['mean_cost'] = np.mean(d['pinsat_adaptive']['cost'])
-    stats['pinsat_adaptive']['std_cost'] = np.std(d['pinsat_adaptive']['cost'])
-    stats['pinsat_adaptive']['mean_time'] = np.mean(d['pinsat_adaptive']['time'])
-    stats['pinsat_adaptive']['std_time'] = np.std(d['pinsat_adaptive']['time'])
-    stats['pinsat_adaptive']['median_time'] = np.median(d['pinsat_adaptive']['time'])
-    stats['pinsat_adaptive']['max_time'] = np.max(d['pinsat_adaptive']['time'])
-    stats['pinsat_adaptive']['mean_state_expansions'] = np.mean(d['pinsat_adaptive']['state_expansions'])
-    stats['pinsat_adaptive']['mean_edge_expansions'] = np.mean(d['pinsat_adaptive']['edge_expansions'])
+    stats['epase']['num_success_problems'] = d['epase']['id'].shape[0]
+    stats['epase']['success_rate'] = pinsat_adaptive_success
+    stats['epase']['mean_cost'] = np.mean(d['epase']['cost'])
+    stats['epase']['std_cost'] = np.std(d['epase']['cost'])
+    stats['epase']['mean_time'] = np.mean(d['epase']['time'])
+    stats['epase']['std_time'] = np.std(d['epase']['time'])
+    stats['epase']['median_time'] = np.median(d['epase']['time'])
+    stats['epase']['max_time'] = np.max(d['epase']['time'])
+    stats['epase']['mean_state_expansions'] = np.mean(d['epase']['state_expansions'])
+    stats['epase']['mean_edge_expansions'] = np.mean(d['epase']['edge_expansions'])
 
 
-    for alg in ['insat', 'pinsat', 'insat_adaptive', 'pinsat_adaptive']:
+    for alg in ['insat', 'pinsat', 'rrt', 'epase']:
         print("------------------")
         print(alg)
         print("------------------")
@@ -182,24 +182,24 @@ def get_intersecting_data(wastar, gepase, pase):
 
 if __name__ == "__main__":
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--num_threads', '-nt', type=int, default=50)
-    # parser.add_argument('--num_data', '-nd', type=int, default=40)
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_threads', '-nt', type=int, default=50)
+    parser.add_argument('--num_data', '-nd', type=int, default=800)
+    args = parser.parse_args()
     
     base_dir = "../logs"
 
-    insat = np.loadtxt(os.path.join(base_dir, "log_insat.txt"))
-    pinsat = np.loadtxt(os.path.join(base_dir, "log_pinsat.txt"))
-    insat_adaptive = np.loadtxt(os.path.join(base_dir, "log_insat_adaptive.txt"))
-    pinsat_adaptive = np.loadtxt(os.path.join(base_dir, "log_pinsat_adaptive.txt"))
+    insat = np.loadtxt(os.path.join(base_dir, "insat_1.txt"))
+    pinsat = np.loadtxt(os.path.join(base_dir, "pinsat_" + str(args.num_threads) + ".txt"))
+    rrt = np.loadtxt(os.path.join(base_dir, "rrt_" + str(args.num_threads) + ".txt"))
+    epase = np.loadtxt(os.path.join(base_dir, "epase_" + str(args.num_threads) + ".txt"))
 
 
     # pdb.set_trace()
     np.set_printoptions(suppress=True)
 
     stats = {}
-    stats[0] = print_stats(insat, pinsat, insat_adaptive, pinsat_adaptive, 0)
+    stats[0] = print_stats(insat, pinsat, rrt, epase, 0)
     # stats[5] =print_stats(wastar, pase, epase, gepase, 5000)
     # stats[10] =print_stats(wastar, pase, epase, gepase, 10000)
     # stats[15] =print_stats(wastar, pase, epase, gepase, 15000)
