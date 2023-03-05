2a13a0690d9375f763da6fcc8e75311ba4654d4f
Shohin Mukherjee <shohin.93@gmail.com>
2023-03-05 18:43:09 -0500

    Fixed wrong scale in a map

--- examples/robot_nav_2d/resources/gen_cost_factor.py
+++ examples/robot_nav_2d/resources/gen_cost_factor.py
@@ -4,9 +4,10 @@ import pdb
 
 dim = {'hrt201n': [305, 294], 'den501d': [338, 320], 'den520d': [257, 256],  'ht_chantry':  [141, 162], 'brc203d': [391, 274]}
 
-scale = 5
+scale_vec = [5, 5, 5, 10, 5]
 
+map_idx = 0
 for map_name, dims in dim.items():
-	print("Map: {} | dims: {}".format(map_name, np.multiply(dims,scale)))
-	cost_mat = np.random.uniform(low=1, high=100, size=np.multiply(dims,scale))
+	print("Map: {} | dims: {}".format(map_name, np.multiply(dims, scale_vec[map_idx])))
+	cost_mat = np.random.uniform(low=1, high=100, size=np.multiply(dims, scale_vec[map_idx]))
 	np.savetxt(os.path.join(map_name, map_name +'_cost_factor.map'), cost_mat, fmt='%d')
