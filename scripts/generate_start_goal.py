3f8abb6e2de144456686934d9780414e60c72b0f
Ramkumar Natarajan <nrkumar93@gmail.com>
2023-03-13 23:38:57 -0400

    updated no fly zones

--- scripts/generate_start_goal.py
+++ scripts/generate_start_goal.py
@@ -74,14 +74,14 @@ goal_file = '../examples/manipulation/resources/shield/goals.txt'
 open(start_file, 'w').close()
 open(goal_file, 'w').close()
 
-allowed = {0: [5, 6, 7],
-           1: [4, 6, 7],
-           2: [4, 5, 7],
-           3: [4, 5, 6],
-           4: [1, 2, 3],
-           5: [0, 2, 3],
-           6: [0, 1, 3],
-           7: [0, 1, 2]}
+allowed = {0: [3, 5, 7],
+           1: [2, 4, 6],
+           2: [1, 5, 7],
+           3: [0, 4, 6],
+           4: [1, 3, 7],
+           5: [0, 2, 6],
+           6: [1, 3, 5],
+           7: [0, 2, 4]}
 
 s = 0
 while s < data_size:
