ac93ff2069dc9b5e1dabd962b3d0bd035acd8296
Ramkumar Natarajan <nrkumar93@gmail.com>
2023-03-15 14:10:51 -0400

    demo script

--- scripts/projectile_demo.py
+++ scripts/projectile_demo.py
@@ -23,7 +23,7 @@ if static_planner:
   dt = 5e-4
 else:
   # dt = 6e-3
-  dt = 9e-4
+  dt = 5e-3
   # dt = 0.5
   # dt = 1
 
@@ -42,7 +42,6 @@ arm_data = MjData(arm_model)
 # for demo
 arm_model_demo = MjModel.from_xml_path(os.path.join(model_dir, mjcf_arm_demo))
 arm_data_demo = MjData(arm_model_demo)
-viewer = mujoco_viewer.MujocoViewer(arm_model_demo, arm_data_demo)
 
 
 def balltraj(end_pos, len, max_len, dx):
@@ -55,11 +54,21 @@ def balltraj(end_pos, len, max_len, dx):
   ball_traj = np.hstack((ball_traj, quat_traj))
   return ball_traj
 
-def processLogToTrajectory(log):
+def processLogToTrajectory(log, goals):
+  traj_list = list()
+  idx = 0
+  sidx = 0
+  eidx = 0
   for i in range(np.shape(log)[0]):
-    
+    if np.array_equal(log[i,:], -1*np.ones((arm_model.nq,))):
+      eidx = i
+      ind_traj = log[sidx:eidx, :]
+      ind_traj = np.vstack([ind_traj, goals[idx,:]])
+      traj_list.append(ind_traj)
+      idx += 1
+      sidx = i+1
 
-  return traj
+  return traj_list
 
 def getBallGoal(goals):
   goal_len = np.shape(goals)[0]
@@ -70,35 +79,50 @@ def getBallGoal(goals):
     ball_goals[go,:] = arm_data.xpos[-1]
   return ball_goals
 
-ind_traj = processLogToTrajectory(traj, starts, goals)
+traj_list = processLogToTrajectory(traj, goals)
 ball_goals = getBallGoal(goals)
 
 
-# ball_traj = list()
-# for i in range(len(ind_traj)):
-#   traj = ind_traj[i]
-#   ball_dx = .02 #m
-#   tot_len = traj.shape[0]
-#   knot = int((tot_len-1)/5)
-#   bt = balltraj(ball_goals[i], np.shape(ind_traj[i])[0], knot+1, ball_dx)
-#   ball_traj.append(bt)
+ball_traj = list()
+for i in range(len(traj_list)):
+  traj = traj_list[i]
+  ball_dx = .02 #m
+  tot_len = traj.shape[0]
+  knot = int((tot_len-1)/5)
+  # bt = balltraj(ball_goals[i], np.shape(traj_list[i])[0], knot+1, ball_dx)
+  bt = balltraj(ball_goals[i], tot_len, tot_len, ball_dx)
+  ball_traj.append(bt)
 
-for i in range(len(ind_traj)):
-  traj = ind_traj[i]
-  # bt = ball_traj[i]
+cfilt_idx = []
+with open('../logs/demo/cfilt_idx.txt', 'r') as filehandle:
+    for line in filehandle:
+        idx = line[:-1]
+        cfilt_idx.append(idx)
+
+# viewer = mujoco_viewer.MujocoViewer(arm_model, arm_data)
+viewer = mujoco_viewer.MujocoViewer(arm_model_demo, arm_data_demo)
+for i in range(len(traj_list)):
+  if i in cfilt_idx:
+    continue
+  traj = traj_list[i]
+  bt = ball_traj[i]
 
   for j in range(np.shape(traj)[0]):
     if viewer.is_alive:
-      arm_data.qpos[:] = traj[j,:]
-      # arm_data.qpos[7:] = bt[j,:]
+      # arm_data.qpos[:] = traj[j,:]
+      arm_data_demo.qpos[:6] = traj[j,:]
+      arm_data_demo.qpos[6:] = bt[j,:]
 
-      mp.mj_step(arm_model, arm_data)
+      # mp.mj_step(arm_model, arm_data)
+      mp.mj_step(arm_model_demo, arm_data_demo)
       viewer.render()
       # sleep(dt)
     else:
         break
 
   sleep(2)
+  if i == len(traj_list)-1:
+    i=0
 
 # close
 viewer.close()
