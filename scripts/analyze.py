3d91cb2d875eda9094754c0e7feca4ccc9d96dfb
Shohin Mukherjee <shohin.93@gmail.com>
2023-03-24 08:58:08 -0400

    Updated analyze to report preprocess success rate

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -10,7 +10,14 @@ def relevant_indices(alg, data, expansion_thresh, time_thresh):
         
     success = np.count_nonzero((data["cost"] > 0) & (data['time'] <= time_thresh))/float(data["cost"].shape[0])
     success_idx = np.array(np.where(data["cost"] != -1))
-    return idx, success, success_idx
+
+    preprocess_success = success
+
+    if 'preprocess_cost' in data.keys():
+        preprocess_success = np.count_nonzero((data["preprocess_cost"] > 0) & (data['time'] <= time_thresh))/float(data["preprocess_cost"].shape[0])
+
+    # pdb.set_trace()
+    return idx, success, success_idx, preprocess_success
 
 
 def prune_failures(data):
@@ -36,15 +43,19 @@ def print_stats(insat, pinsat, rrt, epase, expansion_thresh, time_thresh):
     print("\n\n--------------- Cost thresh: {} ---------------".format(expansion_thresh))
 
     d = {}
-    d['insat'] = {'id' : insat[:, 0], 'time' : insat[:, 1], 'cost' : insat[:, 2], 'length' : insat[:, 3], 'state_expansions' : insat[:, 4], 'edge_expansions' : insat[:, 5]} 
-    d['pinsat'] = {'id' : pinsat[:, 0], 'time' : pinsat[:, 1], 'cost' : pinsat[:, 2], 'length' : pinsat[:, 3], 'state_expansions' : pinsat[:, 4], 'edge_expansions' : pinsat[:, 5]} 
-    d['rrt'] = {'id' : rrt[:, 0], 'time' : rrt[:, 1], 'cost' : rrt[:, 2], 'length' : rrt[:, 3], 'state_expansions' : rrt[:, 4], 'edge_expansions' : rrt[:, 5]} 
-    d['epase'] = {'id' : epase[:, 0], 'time' : epase[:, 1], 'cost' : epase[:, 2], 'length' : epase[:, 3], 'state_expansions' : epase[:, 4], 'edge_expansions' : epase[:, 5]} 
+    d['insat'] = {'id' : insat[:, 0], 'time' : insat[:, 1], 'cost' : insat[:, 2], 
+    'length' : insat[:, 3], 'state_expansions' : insat[:, 4], 'edge_expansions' : insat[:, 5]} 
+    d['pinsat'] = {'id' : pinsat[:, 0], 'time' : pinsat[:, 1], 'cost' : pinsat[:, 2],
+    'length' : pinsat[:, 3], 'state_expansions' : pinsat[:, 4], 'edge_expansions' : pinsat[:, 5]} 
+    d['rrt'] = {'id' : rrt[:, 0], 'time' : rrt[:, 1], 'cost' : rrt[:, 2], 'length' : rrt[:, 3],
+    'state_expansions' : rrt[:, 4], 'edge_expansions' : rrt[:, 5], 'preprocess_cost' : rrt[:, 8]} 
+    d['epase'] = {'id' : epase[:, 0], 'time' : epase[:, 1], 'cost' : epase[:, 2], 'length' : epase[:, 3],
+    'state_expansions' : epase[:, 4], 'edge_expansions' : epase[:, 5], 'preprocess_cost' : epase[:, 8]} 
 
-    insat_idx, insat_success, insat_success_idx = relevant_indices('insat', d['insat'], expansion_thresh, time_thresh)
-    pinsat_idx, pinsat_success, pinsat_success_idx = relevant_indices('pinsat', d['pinsat'], expansion_thresh, time_thresh)
-    rrt_idx, rrt_success, rrt_success_idx = relevant_indices('rrt', d['rrt'], -1, time_thresh)
-    epase_idx, epase_success, epase_success_idx = relevant_indices('epase', d['epase'], -1, time_thresh)
+    insat_idx, insat_success, insat_success_idx, insat_preprocess_success = relevant_indices('insat', d['insat'], expansion_thresh, time_thresh)
+    pinsat_idx, pinsat_success, pinsat_success_idx, pinsat_preprocess_success = relevant_indices('pinsat', d['pinsat'], expansion_thresh, time_thresh)
+    rrt_idx, rrt_success, rrt_success_idx, rrt_preprocess_success = relevant_indices('rrt', d['rrt'], -1, time_thresh)
+    epase_idx, epase_success, epase_success_idx, epase_preprocess_success = relevant_indices('epase', d['epase'], -1, time_thresh)
 
     insat = insat[insat_idx, :]
     pinsat = pinsat[pinsat_idx, :]
@@ -100,6 +111,7 @@ def print_stats(insat, pinsat, rrt, epase, expansion_thresh, time_thresh):
 
     stats['insat']['num_success_problems'] = d['insat']['id'].shape[0]
     stats['insat']['success_rate'] = insat_success
+    stats['insat']['preprocess_success'] = insat_preprocess_success
     stats['insat']['mean_cost'] = np.mean(d['insat']['cost'])
     stats['insat']['std_cost'] = np.std(d['insat']['cost'])
     stats['insat']['mean_time'] = np.mean(d['insat']['time'])
@@ -107,10 +119,13 @@ def print_stats(insat, pinsat, rrt, epase, expansion_thresh, time_thresh):
     stats['insat']['median_time'] = np.median(d['insat']['time'])
     stats['insat']['max_time'] = np.max(d['insat']['time'])
     stats['insat']['mean_state_expansions'] = np.mean(d['insat']['state_expansions'])
+    stats['insat']['median_state_expansions'] = np.median(d['insat']['state_expansions'])
     stats['insat']['mean_edge_expansions'] = np.mean(d['insat']['edge_expansions'])
+
 
     stats['pinsat']['num_success_problems'] = d['pinsat']['id'].shape[0]
     stats['pinsat']['success_rate'] = pinsat_success
+    stats['pinsat']['preprocess_success'] = pinsat_preprocess_success
     stats['pinsat']['mean_cost'] = np.mean(d['pinsat']['cost'])
     stats['pinsat']['std_cost'] = np.std(d['pinsat']['cost'])
     stats['pinsat']['mean_time'] = np.mean(d['pinsat']['time'])
@@ -118,10 +133,12 @@ def print_stats(insat, pinsat, rrt, epase, expansion_thresh, time_thresh):
     stats['pinsat']['median_time'] = np.median(d['pinsat']['time'])
     stats['pinsat']['max_time'] = np.max(d['pinsat']['time'])
     stats['pinsat']['mean_state_expansions'] = np.mean(d['pinsat']['state_expansions'])
+    stats['pinsat']['median_state_expansions'] = np.median(d['pinsat']['state_expansions'])    
     stats['pinsat']['mean_edge_expansions'] = np.mean(d['pinsat']['edge_expansions'])
 
     stats['rrt']['num_success_problems'] = d['rrt']['id'].shape[0]
     stats['rrt']['success_rate'] = rrt_success
+    stats['rrt']['preprocess_success'] = rrt_preprocess_success
     stats['rrt']['mean_cost'] = np.mean(d['rrt']['cost'])
     stats['rrt']['std_cost'] = np.std(d['rrt']['cost'])
     stats['rrt']['mean_time'] = np.mean(d['rrt']['time'])
@@ -133,6 +150,7 @@ def print_stats(insat, pinsat, rrt, epase, expansion_thresh, time_thresh):
 
     stats['epase']['num_success_problems'] = d['epase']['id'].shape[0]
     stats['epase']['success_rate'] = epase_success
+    stats['epase']['preprocess_success'] = epase_preprocess_success
     stats['epase']['mean_cost'] = np.mean(d['epase']['cost'])
     stats['epase']['std_cost'] = np.std(d['epase']['cost'])
     stats['epase']['mean_time'] = np.mean(d['epase']['time'])
@@ -147,8 +165,8 @@ def print_stats(insat, pinsat, rrt, epase, expansion_thresh, time_thresh):
         print("------------------")
         print(alg)
         print("------------------")
-        # for stat in np.sort(list(stats[alg])):
-        for stat in ['num_success_problems', 'success_rate', 'mean_time', 'std_time', 'mean_cost', 'std_cost']:
+        for stat in np.sort(list(stats[alg])):
+        # for stat in ['num_success_problems', 'success_rate', 'mean_time', 'std_time', 'mean_cost', 'std_cost']:
             # if stat in ['mean_time', 'std_time', 'mean_cost', 'std_cost']:
             print('{}: {}'.format(stat, np.round(stats[alg][stat],2)))
             # else:
@@ -199,11 +217,12 @@ if __name__ == "__main__":
     insat = np.loadtxt(os.path.join(base_dir, "insat_smart_1.txt"))
     pinsat = np.loadtxt(os.path.join(base_dir, "pinsat_smart_" + str(args.num_threads) + ".txt"))
     rrt = np.loadtxt(os.path.join(base_dir, "rrtconnect_" + str(args.num_threads) + ".txt"))
-    epase = np.loadtxt(os.path.join(base_dir, "epase_" + str(args.num_threads) + ".txt"))
-    # epase = rrt
+    epase = np.loadtxt(os.path.join(base_dir, "gepase_" + str(args.num_threads) + ".txt"))
+
+    pdb.set_trace()    
 
     np.set_printoptions(suppress=True)
 
     stats = {}
-    stats[0] = print_stats(insat, pinsat, rrt, epase, 1, 10)
+    stats[0] = print_stats(insat, pinsat, rrt, epase, 0, 20)
 
